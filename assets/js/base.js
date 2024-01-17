338e5a1b0047c919680df6dce6a13c9cde439747
Jeffrey Overmeer <jeffrey.overmeer@gmail.com>
2024-01-17 20:55:30 +0100

    first

---
+++ assets/js/base.js
@@ -0,0 +1,14 @@
+// base.js
+// Voeg hier je verwijzingen naar andere JavaScript-bestanden toe
+// bijvoorbeeld:
+
+import './layout.js'
+import './data.js';
+// import './footer.js';
+
+// Voeg ook je eigen JavaScript-functionaliteit hier toe, indien nodig
+console.log('base.js is geladen!');
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  console.log('init')
+});
